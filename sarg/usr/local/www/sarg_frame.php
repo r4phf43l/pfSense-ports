cefb753e318bef16008ed2781c7952612280b5a7
r4phf43l <61799987+r4phf43l@users.noreply.github.com>
2024-02-29 17:12:21 -0300

    Update sarg_frame.php

--- sarg/usr/local/www/sarg_frame.php
+++ sarg/usr/local/www/sarg_frame.php
@@ -2,8 +2,8 @@
 /*
 	sarg_frame.php
 	part of pfSense (https://www.pfSense.org/)
+ 	Copyright (C) 2024 Rafael de Almeida Antonio <rafael.antonio@gmail.com>
 	Copyright (C) 2012 Marcello Coutinho <marcellocoutinho@gmail.com>
-  Copyright (C) 2024 Rafael de Almeida Antonio (R4PHF43L) <rafael.aantonio@gmail.com>
 	Copyright (C) 2015 ESF, LLC
 	All rights reserved.
 
@@ -32,12 +32,12 @@ require_once("authgui.inc");
 
 $uname = posix_uname();
 if ($uname['machine'] == 'amd64') {
-	ini_set('memory_limit', '250M');
+	ini_set('memory_limit', '768M');
 }
 
 // local file inclusion check
 if(!empty($_REQUEST['file'])){
-    $_REQUEST['file']=preg_replace('/(\.+\/|\\\.*|\/{2,})*/',"", $_REQUEST['file']);
+        $_REQUEST['file']=preg_replace('/(\.+\/|\\\.*|\/{2,})*/',"", $_REQUEST['file']);
 }
 
 if (preg_match("/(\S+)\W(\w+.html)/", $_REQUEST['file'], $matches)) {
@@ -70,24 +70,24 @@ if (file_exists("{$dir}/{$url}")) {
 	unset ($data);
 }
 if ($report != "" ) {
-    $pattern[0] = '/href=\W(\S+html)\W/';
+$pattern[0] = '/href=\W(\S+html)\W/';
     $replace[0] = "href=/sarg_frame.php?dir=" . $dsuffix . "&prevent=" . $rand . "&file=$prefix/$1";
     $pattern[1] = '/<head>/';
     $replace[1] = '<head><META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE"><META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE">';
-    $pattern[2] = '/tt.html/';
-    $replace[2] = 'tt.html&';
     $pattern[26] = '/img src="\S+\W([a-zA-Z0-9.-]+.png)/';
     $replace[26] = 'img src="/sarg-images/$1';
     $pattern[27] = '@img src="([.a-z/]+)/(\w+\.\w+)@';
     $replace[27] = 'img src="/sarg-images' . $prefix . '/$1/$2';
     $pattern[28] = '/img src="([a-zA-Z0-9.-_]+).png/';
     $replace[28] = 'img src="/sarg-images/temp/$1.' . $rand . '.png';
+    $pattern[2] = '/tt.html/';
+    $replace[2] = 'tt.html&';
   
     //R4PHF4 Customs
     $pattern[3] = '/<(\/?table)[^>]*class=\"sortable\"[^>]*>/';
     $replace[3] = '<$1 cellpadding="1" cellspacing="2" class="table table-striped table-hover table-condensed sortable-theme-bootstrap" data-sortable="" data-sortable-initialized="true">';
     $pattern[4] = '/<body>/';
-    $replace[4] = '<link rel="stylesheet" href="/vendor/font-awesome/css/all.min.css?v=' . $rand . '"><link rel="stylesheet" href="/vendor/font-awesome/css/v4-shims.css?v=' . $rand . '"><link rel="stylesheet" href="/vendor/sortable/sortable-theme-bootstrap.css?v=' . $rand . '"><link rel="stylesheet" href="/css/pfSense.css?v=' . $rand . '"><body>';
+    $replace[4] = '<link rel="stylesheet" href="/vendor/font-awesome/css/all.min.css?v=' . $rand . '"><link rel="stylesheet" href="/vendor/font-awesome/css/v4-shims.css?v=' . $rand . '"><link rel="stylesheet" href="/vendor/sortable/sortable-theme-bootstrap.css?v=' . $rand . '"><link rel="stylesheet" href="/css/pfSense.css?v=' . $rand . '"><script type="text/javascript" src="/sarg_print.js"></script><body>';
     $pattern[5] = '/header_[lc]/';
     $replace[5] = '';
     $pattern[6] = '/<\/thead>/';
@@ -101,7 +101,7 @@ if ($report != "" ) {
     $pattern[10] = '/<\/style>/';
     $replace[10] = '</style>-->';
     $pattern[11] = '/<table[^>]*class=\"(report|sortable)\"[^>]*>/';
-    $replace[11] = '<table cellpadding="0" cellspacing="2" class="table table-striped table-hover table-condensed sortable-theme-bootstrap" data-sortable="" data-sortable-initialized="true">';
+    $replace[11] = "<table cellpadding=\"0\" cellspacing=\"2\" class=\"$1 table table-striped table-hover table-condensed sortable-theme-bootstrap\" data-sortable=\"\" data-sortable-initialized=\"true\">";
     $pattern[12] = '/<table cellpadding=\"0\" cellspacing=\"2\">/';
     $replace[12] = '<table cellpadding="0" cellspacing="2" class="table table-striped table-hover table-condensed sortable-theme-bootstrap" data-sortable="" data-sortable-initialized="true">';
     $pattern[13] = '/<th class=\"title_c\"\>([^>]*)\<\/th\>/';
@@ -126,20 +126,33 @@ if ($report != "" ) {
     $replace[22] = '<tr class="btn btn-default btn-sm"><td colspan="0">';
     $pattern[23] = '/<div class=\"report\"><table width=\"100%\" cellpadding=\"1\" cellspacing=\"2\">/';
     $replace[23] = '<div class="report content"><table width="100%" cellpadding="1" cellspacing="2" style="display: flex" class="btn-group">';
+
     $pattern[24] = '/<tr><td class=\"data\">(.*?)<td class=\"data\">DENIED<\/td><\/tr>/';
     $replace[24] = "<tr><td class=\"data\"><i class=\"fa fa-times-circle text-danger icon-primary\" title=\"DENIED\"></i>&nbsp;$1</tr>";
-    $pattern[25] = '/<tr><td class=\"data\"><a href=\/sarg_(.*?)<\/td><td>(.*?)<\/td><td (.*?)>(.*?)<\/td><td (.*?)>(.*?)<\/td><td (.*?)>(.*?)<\/td><td (.*?)>(.*?)<\/td><td (.*?)>(.*?)<\/td><td (.*?)>(.*?)<\/td><td (.*?)>(.*?)<\/td><td class=\"data\">(.*?)<\/td><\/tr>/';
-    $replace[25] = "<tr><td class=\"data\"><i class=\"text-success fa fa-check-circle fa-1x\" title=\"ACCESS\"></i>&nbsp;<a href=\/sarg_$1</td><td>$2</td><td $3>$4</td><td $5>$6</td><td $7>$8</td><td $9>$10</td><td $11>$12</td><td $13>$14</td><td $15>$16</td><td class=\"data\">$17</td></tr>";
 
-    // Look for graph files inside reports.
-    if (preg_match_all('/img src="([a-zA-Z0-9._-]+).png/', $report, $images)) {
-        conf_mount_rw();
-        foreach ($images[1] as $image) {
-            copy("{$dir}/{$prefix}/{$image}.png", "/usr/local/www/sarg-images/temp/{$image}.{$rand}.png");
-        }
-        conf_mount_ro();
-    }
-    
+    $pattern[25] = '/<tr><td class=\"data\"><a href=\/sarg_(.*?)<\/td><td>(.*?)<\/td><td (.*?)>(.*?)<\/td><td (.*?)>(.*?)<\/td><td (.*?)>(.*?)<\/td><td (.*?)>(.*?)<\/td><td (.*?)>(.*?)<\/td><td (.*?)>(.*?)<\/td><td (.*?)>(.*?)<\/td><td class=\"data\">(.*?)<\/td><\/tr>/';
+    $replace[25] = "<tr><td class=\"data\"><i class=\"text-success fa fa-check-circle fa-1x\" title=\"ACCESS\"></i>&nbsp;<a href=/sarg_$1</td><td>$2</td><td $3>$4</td><td $5>$6</td><td $7>$8</td><td $9>$10</td><td $11>$12</td><td $13>$14</td><td $15>$16</td><td class=\"data\">$17</td></tr>";
+
+    $pattern[29] = '/<div class="report"><table width="100%"/';
+    $replace[29] = '<div class="report"><div class="content print-group btn-group pull-right"><a href="javascript:exportToCSV(\'printable\')" class="btn btn-default btn-sm">Excel</a><a href="javascript:exportToCSV(\'printable\', \',\')" class="btn btn-default btn-sm">CSV</a><a href="javascript:printPage()" class="btn btn-default btn-sm">Print</a></div><table id="printable" width="100%"';
+
+    $pattern[30] = '/<tr><td><img src=\"(\S+\W[a-zA-Z0-9.-]+.png)\" alt=\"B\"><\/td><\/tr>/';
+    $replace[30] = "<tr class=\"panel-default\"><td class=\"panel-heading\"><h2 class=\"panel-title\">E2guardian User Access Reports</h2></td></tr><tr><td><h2 class=\"panel-title\"><a href=\"javascript:history.go(-1)\"><i class=\"fa fa-arrow-left text-primary\" title=\"Back\"></i></a>&nbsp;Graph report</h2><div class=\"content print-group btn-group pull-right\"><a href=\"javascript:printPage()\" class=\"btn btn-default btn-sm\">Print</a></div></td></tr><tr><td><img src=\"$1\" alt=\"B\"></td></tr>";
+
+    $pattern[31] = '/<tr><(td|th) class=\"\" colspan=\"2\">(Period|User)/';
+    $replace[31] = "<tr><$1 class=\"content\" colspan=\"2\">$2";
+
+    $pattern[32] = '/<tr><(.*?)>Sort:(.*?)<\/td><\/tr>/';
+    $replace[32] = "";
+
+	// look for graph files inside reports. 
+	if (preg_match_all('/img src="([a-zA-Z0-9._-]+).png/', $report, $images)) {
+		conf_mount_rw();
+		for ($x = 0; $x < count($images[1]); $x++) {
+			copy("{$dir}/{$prefix}/{$images[1][$x]}.png", "/usr/local/www/sarg-images/temp/{$images[1][$x]}.{$rand}.png");
+		}
+		conf_mount_ro();
+	}
 	print preg_replace($pattern, $replace, $report);
 } else {
 	print "Error: Could not find report index file.<br />Check and save Sarg settings and try to force Sarg schedule.";
